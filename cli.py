592fd8fb61d669cc7b929ae91f9c2d2624a59dcd
KillianLucas <63927363+KillianLucas@users.noreply.github.com>
2023-07-26 02:25:52 +0000

    Renamed `auto_exec` to `no_confirm`

--- cli.py
+++ cli.py
@@ -3,13 +3,14 @@ import interpreter
 
 def main():
     parser = argparse.ArgumentParser(description='Chat with Open Interpreter.')
-    parser.add_argument('--auto_exec', action='store_true', help='Enable auto execution')
+    parser.add_argument('--no_confirm', action='store_true', help='Execute code without user confirmation')
     args = parser.parse_args()
 
     # Set safe_mode on the imported interpreter instance
-    if args.auto_exec:
-      print("\nYou are in auto execution mode. Generated code will execute without confirmation.\n")
-      interpreter.auto_exec = True
+    if args.no_confirm:
+      interpreter.no_confirm = True
+    else:
+      print("\nGenerated code will require confirmation before executing. Run `interpreter --no_confirm` for instant code execution.\n")
     
     # Now run the chat method
     interpreter.chat()
