9337bd280f19ad86f45bb50468971feabcba9421
KillianLucas <63927363+KillianLucas@users.noreply.github.com>
2023-08-05 18:56:05 +0000

    Better code parsing, disabled active line display for Bash

--- interpreter/code_interpreter.py
+++ interpreter/code_interpreter.py
@@ -1,12 +1,23 @@
 import subprocess
 import threading
 import time
+import ast
+import astor
 
 # Mapping of languages to their start and print commands
 language_map = {
-    "python": {"start_cmd": "python -i -q -u", "print_cmd": 'print("{}")'},
-    "bash": {"start_cmd": "bash --noediting", "print_cmd": 'echo "{}"'},
-    "javascript": {"start_cmd": "node -i", "print_cmd": 'console.log("{}")'}
+  "python": {
+    "start_cmd": "python -i -q -u",
+    "print_cmd": 'print("{}")'
+  },
+  "bash": {
+    "start_cmd": "bash --noediting",
+    "print_cmd": 'echo "{}"'
+  },
+  "javascript": {
+    "start_cmd": "node -i",
+    "print_cmd": 'console.log("{}")'
+  }
 }
 
 
@@ -20,6 +31,7 @@ class CodeInterpreter:
   def __init__(self, language):
     self.language = language
     self.proc = None
+    self.active_line = None
 
   def start_process(self):
     # Get the start_cmd for the selected language
@@ -43,45 +55,33 @@ class CodeInterpreter:
   def run(self):
 
     self.code = self.active_block.code
-    
+
     if not self.proc:
       self.start_process()
 
     # Reset output
     self.output = ""
 
-    # Split the code into lines and add print statements to track the active line
-    code_lines = self.code.strip().split('\n')
-    self.active_line = 0
-
     # Use the print_cmd for the selected language
-    print_cmd = language_map[self.language]["print_cmd"]
+    self.print_cmd = language_map[self.language]["print_cmd"]
+    code = self.code
 
-    # Initialize an empty list to hold the modified lines of code
-    modified_code_lines = []
-
-    # Iterate over each line in the original code
-    for i, line in enumerate(code_lines):
-      # Get the leading whitespace of the current line
-      leading_whitespace = line[:len(line) - len(line.lstrip())]
-
-      # Format the print command with the current line number
-      print_line = print_cmd.format(f"ACTIVE_LINE:{i+1}")
-
-      # Prepend the leading whitespace to the print command
-      print_line = leading_whitespace + print_line
-
-      # Add the print command and the original line to the modified lines
-      modified_code_lines.append(print_line)
-      modified_code_lines.append(line)
-
-    # Join the modified lines with newlines
-    code = "\n".join(modified_code_lines)
+    # If it's Python, insert print commands to say what line is running and fix indentation
+    if self.language == "python":
+      print("CODE:\n", code)
+      code = self.add_active_line_prints(code)
+      print("ACODE:\n", code)
+      code = normalize(code)
+      print("NCODE:\n", code)
+      code = fix_code_indentation(code)
+      print("FCODE:\n", code)
 
     # Add end command
-    code += "\n\n" + print_cmd.format('END') + "\n"
-    
-    print("Running:\n---\n", code, "\n---")
+    code += "\n\n" + self.print_cmd.format('END') + "\n"
+
+    print("Running code:")
+    print(code)
+    print("---")
 
     # Reset self.done so we can .wait() for it
     self.done = threading.Event()
@@ -99,6 +99,43 @@ class CodeInterpreter:
 
     # Return code output
     return self.output
+
+  def add_active_line_prints(self, code):
+    """
+    This function takes a code snippet and adds print statements before each line,
+    indicating the active line number during execution. The print statements respect
+    the indentation of the original code, using the indentation of the next non-blank line.
+    """
+    
+    # Split the original code into lines
+    code_lines = code.strip().split('\n')
+
+    # Initialize an empty list to hold the modified lines of code
+    modified_code_lines = []
+
+    # Iterate over each line in the original code
+    for i, line in enumerate(code_lines):
+        # Initialize a variable to hold the leading whitespace of the next non-empty line
+        leading_whitespace = ""
+
+        # Iterate over the remaining lines to find the leading whitespace of the next non-empty line
+        for next_line in code_lines[i:]:
+            if next_line.strip():
+                leading_whitespace = next_line[:len(next_line) - len(next_line.lstrip())]
+                break
+
+        # Format the print command with the current line number, using the found leading whitespace
+        print_line = self.print_cmd.format(f"ACTIVE_LINE:{i+1}")
+        print_line = leading_whitespace + print_line
+
+        # Add the print command and the original line to the modified lines
+        modified_code_lines.append(print_line)
+        modified_code_lines.append(line)
+
+    # Join the modified lines with newlines and return the result
+    code = "\n".join(modified_code_lines)
+    return code
+
 
   def save_and_display_stream(self, stream):
     # Handle each line of output
@@ -124,6 +161,26 @@ class CodeInterpreter:
       self.active_block.output = self.output
       self.active_block.refresh()
 
+def normalize(code):
+    # Parse the code into an AST
+    parsed_ast = ast.parse(code)
+
+    # Convert the AST back to source code
+    return astor.to_source(parsed_ast)
+
+def fix_code_indentation(code):
+    lines = code.split("\n")
+    fixed_lines = []
+    was_indented = False
+    for line in lines:
+        current_indent = len(line) - len(line.lstrip())
+        if current_indent == 0 and was_indented:
+            fixed_lines.append('') # Add an empty line after an indented block
+        fixed_lines.append(line)
+        was_indented = current_indent > 0
+
+    return "\n".join(fixed_lines)
+
 def truncate_output(data):
 
   # In the future, this will come from a config file
@@ -138,4 +195,5 @@ def truncate_output(data):
   # If data exceeds max length, truncate it and add message
   if len(data) > max_output_chars:
     data = message + data[-max_output_chars:]
+
   return data
