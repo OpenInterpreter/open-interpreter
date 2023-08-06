7fa1fd93a28a88a89f6107a6a406e6daaf82e650
KillianLucas <63927363+KillianLucas@users.noreply.github.com>
2023-08-06 09:05:43 +0000

    Experimenting with Llama-2 support

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,20 +1,24 @@
 import interpreter
-interpreter.no_confirm = True
+interpreter.auto_run = True
 interpreter.temperature = 0
 
 def test_delayed_exec():
     interpreter.reset()
     interpreter.chat("""Can you write a single block of code and run_code it that prints something, then delays 5 seconds, then prints something else? No talk just code. Thanks!""", return_messages=True)
 
+def test_nested_loops_and_multiple_newlines():
+    interpreter.reset()
+    interpreter.chat("""Can you write a nested for loop in python and shell and run them? Also put 1-3 newlines between each line in the code. Thanks!""", return_messages=True)
+
 def test_math():
     interpreter.reset()
-    messages = interpreter.chat("""Please perform the calculation 27073*7397 then reply with just the integer answer, nothing else.""", return_messages=True)
+    messages = interpreter.chat("""Please perform the calculation 27073*7397 then reply with just the integer answer with no commas or anything, nothing else.""", return_messages=True)
     assert messages[-1] == {'role': 'assistant', 'content': '200258981'}
 
 def test_hello_world():
     interpreter.reset()
     messages = interpreter.chat("""Please reply with just the words "Hello, World!" and nothing else.""", return_messages=True)
-    assert messages == [{'role': 'user', 'content': 'Please reply with just the words "Hello, World!" and nothing else.'}, {'role': 'assistant', 'content': 'Hello, World!'}]
+    assert messages == [{'role': 'user', 'content': 'Please reply with just the words "Hello, World!" and nothing else. Do not run code.'}, {'role': 'assistant', 'content': 'Hello, World!'}]
   
 def test_markdown():
     interpreter.reset()
