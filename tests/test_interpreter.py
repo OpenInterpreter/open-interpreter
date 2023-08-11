5932cf321cb64d1d870d66b6d7ac86fef93523b2
KillianLucas <63927363+KillianLucas@users.noreply.github.com>
2023-08-11 03:50:02 +0000

    return_messages now works for non-interactive chats

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -2,6 +2,17 @@ import interpreter
 interpreter.auto_run = True
 interpreter.temperature = 0
 
+def test_math():
+    interpreter.reset()
+    messages = interpreter.chat("""Please perform the calculation 27073*7397 then reply with just the integer answer with no commas or anything, nothing else.""", return_messages=True)
+    print(messages)
+    assert messages[-1] == {'role': 'assistant', 'content': '200258981'}
+
+def test_hello_world():
+    interpreter.reset()
+    messages = interpreter.chat("""Please reply with just the words "Hello, World!" and nothing else.""", return_messages=True)
+    assert messages == [{'role': 'user', 'content': 'Please reply with just the words "Hello, World!" and nothing else.'}, {'role': 'assistant', 'content': 'Hello, World!'}]
+
 def test_delayed_exec():
     interpreter.reset()
     interpreter.chat("""Can you write a single block of code and run_code it that prints something, then delays 1 second, then prints something else? No talk just code. Thanks!""", return_messages=True)
@@ -10,16 +21,6 @@ def test_nested_loops_and_multiple_newlines():
     interpreter.reset()
     interpreter.chat("""Can you write a nested for loop in python and shell and run them? Also put 1-3 newlines between each line in the code. Thanks!""", return_messages=True)
 
-def test_math():
-    interpreter.reset()
-    messages = interpreter.chat("""Please perform the calculation 27073*7397 then reply with just the integer answer with no commas or anything, nothing else.""", return_messages=True)
-    assert messages[-1] == {'role': 'assistant', 'content': '200258981'}
-
-def test_hello_world():
-    interpreter.reset()
-    messages = interpreter.chat("""Please reply with just the words "Hello, World!" and nothing else.""", return_messages=True)
-    assert messages == [{'role': 'user', 'content': 'Please reply with just the words "Hello, World!" and nothing else. Do not run code.'}, {'role': 'assistant', 'content': 'Hello, World!'}]
-  
 def test_markdown():
     interpreter.reset()
     interpreter.chat("Hi, can you test out a bunch of markdown features? Try writing a fenced code block, a table, headers, everything. DO NOT write the markdown inside a markdown code block, just write it raw.")
