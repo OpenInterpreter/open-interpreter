1c0c4bea99d18c1ff317b16c3c5b49f9f4a8bcfb
KillianLucas <63927363+KillianLucas@users.noreply.github.com>
2023-08-28 20:30:24 +0000

    Code Llama

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -163,7 +163,7 @@ class Interpreter:
     # If self.local, we actually don't use self.model
     # (self.auto_run is like advanced usage, we display no messages)
     if not self.local and not self.auto_run:
-      welcome_message += f"\n> Model set to `{self.model.upper()}`"
+      welcome_message += f"\n> Model set to `{self.model.upper()}`\n> **Tip:** To run locally, use `interpreter --local`"
 
     if self.local:
       welcome_message += f"\n> Model set to `Llama-2`"
