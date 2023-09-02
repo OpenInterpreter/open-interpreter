35655d8177bf5d14522b7010a09593ad6973a44d
Tanmay Jain <85993243+TanmayDoesAI@users.noreply.github.com>
2023-09-02 20:28:05 +0530

    changing the tip

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -160,7 +160,7 @@ class Interpreter:
     # If self.local, we actually don't use self.model
     # (self.auto_run is like advanced usage, we display no messages)
     if not self.local and not self.auto_run:
-      welcome_message += f"\n> Model set to `{self.model.upper()}`\n\n**Tips:**\n To run locally, use `interpreter --local`\n To run it with GPT-3.5, use `interpreter --fast`"
+      welcome_message += f"\n> Model set to `{self.model.upper()}`\n\n**Tips:**\n To run locally, use `interpreter --local`\n To run it with GPT-3.5 (Faster), use `interpreter --fast`"
 
     if self.local:
       welcome_message += f"\n> Model set to `Code-Llama`"
