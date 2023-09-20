24dead248b1ae81076086cdd997cca574a34d5cc
RowDog <mrdiamonddirt@gmail.com>
2023-09-20 13:30:46 +0100

    much simpler tts and speech recognition

--- interpreter/voice.py
+++ interpreter/voice.py
@@ -1,54 +1,43 @@
-import time
-import threading
-from mutagen.mp3 import MP3
-from gtts import gTTS
 import speech_recognition as SR
-import pygame
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-
+import pyttsx3
 
 class SpeechAssistant:
     def __init__(self, wakeup_word="interpreter"):
         self.wakeup_word = wakeup_word
         self.speech_recognizer = SR.Recognizer()
+        self.engine = pyttsx3.init()
 
     def tts_and_play_audio(self, text):
-        language = 'en'
-        speech = gTTS(text=text, lang=language, slow=False, tld='com.au')
-        speech.save("speech.mp3")
-        self.play_audio("speech.mp3")
-        os.remove("speech.mp3")
-
-    def play_audio(self, file_path):
-        pygame.mixer.init()
-        pygame.mixer.music.load(file_path)
-        pygame.mixer.music.play()
-        audio = MP3(file_path)
-        time.sleep(audio.info.length)
-        pygame.mixer.music.stop()
-        pygame.mixer.quit()
-        # Delete the file after playing it
+        # stop if it is already speaking
+        if self.engine._inLoop:
+            print("inloop")
+            self.engine.endLoop()
+            self.engine.stop()
+        self.engine.say(text)
+        self.engine.runAndWait()
 
     def start_speech_recognition(self):
         while True:
-             print("Listening for wakeup word...")
-             with SR.Microphone() as mic:
-                   try:
-                        self.speech_recognizer.adjust_for_ambient_noise(
-                            mic, duration=0.4)
-                        audio = self.speech_recognizer.listen(mic)
-                        text = self.speech_recognizer.recognize_google(
-                            audio, language="en-US")
-                        text = str(text).lower()
-                        if self.wakeup_word in text:
-                            print("Wakeup word", self.wakeup_word, "detected!")
-                            print("You said:", text)
-                            return text
-                   except SR.UnknownValueError:
-                        pass
-                   except Exception as e:
-                        print("Error:", str(e))
+            print("Listening for wakeup word...")
+            with SR.Microphone() as mic:
+                try:
+                    self.speech_recognizer.adjust_for_ambient_noise(
+                        mic, duration=0.4)
+                    audio = self.speech_recognizer.listen(mic)
+
+                    result = self.speech_recognizer.recognize_whisper(
+                        audio, "base")
+                    text = result
+                    if self.wakeup_word in text:
+                        print("Wakeup word", self.wakeup_word, "detected!")
+                        print("You said:", text)
+                        return text
+                except SR.UnknownValueError:
+                    pass
+                except Exception as e:
+                    print("Error:", str(e))
+                finally:
+                    pass
 
 
 if __name__ == "__main__":
