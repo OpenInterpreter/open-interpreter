5df87aa4b87fa4d8e08561339c7f752babcc34a8
killian <63927363+KillianLucas@users.noreply.github.com>
2023-09-20 19:27:17 -0700

    Basics

--- interpreter/core/setup.py
+++
@@ -1,6 +0,0 @@
-"""
-Set everything up.
-
-If we're in CLI mode, use config.yaml in the appdir to load all attributes.
-Then, use a wizard to get any needed information.
-"""
---
+++ interpreter/llm/function_call_utils.py
@@ -0,0 +1,5 @@
+"""
+Modifies the system message whether or not it's a function-calling or non-function-calling LLM.
+
+Utils for capturing "function calls" from non-function-calling LLMs are also here.
+"""
