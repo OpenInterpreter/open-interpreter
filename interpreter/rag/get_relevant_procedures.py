81684c3a2ed12f8cbb79d964a1824b41dead2771
kubla <michael.tiffany@gmail.com>
2023-09-24 08:40:32 -0400

    Update get_relevant_procedures.py
    Fixed a typo in the instructions to the model

--- interpreter/rag/get_relevant_procedures.py
+++ interpreter/rag/get_relevant_procedures.py
@@ -10,6 +10,6 @@ def get_relevant_procedures(messages):
     url = "https://open-procedures.replit.app/search/"
 
     relevant_procedures = requests.get(url, json=query).json()["procedures"]
-    relevant_procedures = "[Recommended Procedures]\n" + "\n---\n".join(relevant_procedures) + "\nIn your plan, include steps and, if present, **EXACT CODE SNIPPETS** (especially for depracation notices, **WRITE THEM INTO YOUR PLAN -- underneath each numbered step** as they will VANISH once you execute your first line of code, so WRITE THEM DOWN NOW if you need them) from the above procedures if they are relevant to the task. Again, include **VERBATIM CODE SNIPPETS** from the procedures above if they are relevent to the task **directly in your plan.**"
+    relevant_procedures = "[Recommended Procedures]\n" + "\n---\n".join(relevant_procedures) + "\nIn your plan, include steps and, if present, **EXACT CODE SNIPPETS** (especially for deprecation notices, **WRITE THEM INTO YOUR PLAN -- underneath each numbered step** as they will VANISH once you execute your first line of code, so WRITE THEM DOWN NOW if you need them) from the above procedures if they are relevant to the task. Again, include **VERBATIM CODE SNIPPETS** from the procedures above if they are relevent to the task **directly in your plan.**"
 
     return relevant_procedures
