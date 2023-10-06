46d34e3cb04af5ccc30d3204fdd9d77128c53669
Tony Okeke <tonykabilanokeke@gmail.com>
2023-10-06 17:51:06 -0400

    feat: Enable API Logging with Helicone
    API requests will be logged via Helicone when the `HELICONE_API_KEY` variable is set

--- interpreter/core/respond.py
+++ interpreter/core/respond.py
@@ -6,6 +6,14 @@ from ..rag.get_relevant_procedures import get_relevant_procedures
 from ..utils.truncate_output import truncate_output
 import traceback
 import litellm
+import os
+
+
+# Enable logging with Helicone
+if os.getenv("HELICONE_API_KEY"):
+    litellm.api_base = "https://oai.hconeai.com/v1"
+    litellm.headers = {"Helicone-Auth": f"Bearer {os.getenv('HELICONE_API_KEY')}"}
+
 
 def respond(interpreter):
     """
@@ -14,21 +22,22 @@ def respond(interpreter):
     """
 
     while True:
-
         ### PREPARE MESSAGES ###
 
         system_message = interpreter.system_message
-        
+
         # Open Procedures is an open-source database of tiny, up-to-date coding tutorials.
         # We can query it semantically and append relevant tutorials/procedures to our system message
         get_relevant_procedures(interpreter.messages[-2:])
         if not interpreter.local:
             try:
-                system_message += "\n\n" + get_relevant_procedures(interpreter.messages[-2:])
+                system_message += "\n\n" + get_relevant_procedures(
+                    interpreter.messages[-2:]
+                )
             except:
                 # This can fail for odd SLL reasons. It's not necessary, so we can continue
                 pass
-        
+
         # Add user info to system_message, like OS, CWD, etc
         system_message += "\n\n" + get_user_info_string()
 
@@ -44,7 +53,6 @@ def respond(interpreter):
             if "output" in message and message["output"] == "":
                 message["output"] = "No output"
 
-
         ### RUN THE LLM ###
 
         # Add a new message from the assistant to interpreter's "messages" attribute
@@ -55,7 +63,6 @@ def respond(interpreter):
         # + yielding chunks to the user
         try:
             for chunk in interpreter._llm(messages_for_llm):
-
                 # Add chunk to the last message
                 interpreter.messages[-1] = merge_deltas(interpreter.messages[-1], chunk)
 
@@ -63,29 +70,30 @@ def respond(interpreter):
                 # It will yield dict with either a message, language, or code (or language AND code)
                 yield chunk
         except litellm.exceptions.BudgetExceededError:
-            display_markdown_message(f"""> Max budget exceeded
+            display_markdown_message(
+                f"""> Max budget exceeded
 
                 **Session spend:** ${litellm._current_cost}
                 **Max budget:** ${interpreter.max_budget}
 
                 Press CTRL-C then run `interpreter --max_budget [higher USD amount]` to proceed.
-            """)
+            """
+            )
             break
         # Provide extra information on how to change API keys, if we encounter that error
         # (Many people writing GitHub issues were struggling with this)
         except Exception as e:
-            if 'auth' in str(e).lower() or 'api key' in str(e).lower():
+            if "auth" in str(e).lower() or "api key" in str(e).lower():
                 output = traceback.format_exc()
-                raise Exception(f"{output}\n\nThere might be an issue with your API key(s).\n\nTo reset your API key (we'll use OPENAI_API_KEY for this example, but you may need to reset your ANTHROPIC_API_KEY, HUGGINGFACE_API_KEY, etc):\n        Mac/Linux: 'export OPENAI_API_KEY=your-key-here',\n        Windows: 'setx OPENAI_API_KEY your-key-here' then restart terminal.\n\n")
+                raise Exception(
+                    f"{output}\n\nThere might be an issue with your API key(s).\n\nTo reset your API key (we'll use OPENAI_API_KEY for this example, but you may need to reset your ANTHROPIC_API_KEY, HUGGINGFACE_API_KEY, etc):\n        Mac/Linux: 'export OPENAI_API_KEY=your-key-here',\n        Windows: 'setx OPENAI_API_KEY your-key-here' then restart terminal.\n\n"
+                )
             else:
                 raise
-        
-        
-        
+
         ### RUN CODE (if it's there) ###
 
         if "code" in interpreter.messages[-1]:
-            
             if interpreter.debug_mode:
                 print("Running code:", interpreter.messages[-1])
 
@@ -94,7 +102,9 @@ def respond(interpreter):
                 code = interpreter.messages[-1]["code"]
 
                 # Fix a common error where the LLM thinks it's in a Jupyter notebook
-                if interpreter.messages[-1]["language"] == "python" and code.startswith("!"):
+                if interpreter.messages[-1]["language"] == "python" and code.startswith(
+                    "!"
+                ):
                     code = code[1:]
                     interpreter.messages[-1]["code"] = code
                     interpreter.messages[-1]["language"] = "shell"
@@ -102,7 +112,9 @@ def respond(interpreter):
                 # Get a code interpreter to run it
                 language = interpreter.messages[-1]["language"]
                 if language not in interpreter._code_interpreters:
-                    interpreter._code_interpreters[language] = create_code_interpreter(language)
+                    interpreter._code_interpreters[language] = create_code_interpreter(
+                        language
+                    )
                 code_interpreter = interpreter._code_interpreters[language]
 
                 # Yield a message, such that the user can stop code execution if they want to
