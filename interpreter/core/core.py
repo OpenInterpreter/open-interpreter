ead2a6d6426a446f54019ed8fa2d425fdd6afa12
killian <63927363+KillianLucas@users.noreply.github.com>
2023-10-10 13:50:26 -0700

    Removed /archive, adding Mistral support

--- interpreter/core/core.py
+++ interpreter/core/core.py
@@ -47,6 +47,7 @@ class Interpreter:
         self.api_key = None
         self.max_budget = None
         self._llm = None
+        self.gguf_quality = None
 
         # Load config defaults
         config = get_config()
