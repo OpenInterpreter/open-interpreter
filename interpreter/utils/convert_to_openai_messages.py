187cb8f41725bf916776aeaef16a036220e49278
killian <63927363+KillianLucas@users.noreply.github.com>
2023-10-11 14:28:18 -0700

    OpenAI messages -> text LLMs are now non-function-calling

--- interpreter/utils/convert_to_openai_messages.py
+++ interpreter/utils/convert_to_openai_messages.py
@@ -1,6 +1,6 @@
 import json
 
-def convert_to_openai_messages(messages):
+def convert_to_openai_messages(messages, function_calling=True):
     new_messages = []
 
     for message in messages:  
@@ -13,29 +13,37 @@ def convert_to_openai_messages(messages):
             new_message["content"] = message["message"]
 
         if "code" in message:
-            new_message["function_call"] = {
-                "name": "run_code",
-                "arguments": json.dumps({
-                    "language": message["language"],
-                    "code": message["code"]
-                }),
-                # parsed_arguments isn't actually an OpenAI thing, it's an OI thing.
-                # but it's soo useful! we use it to render messages to text_llms
-                "parsed_arguments": {
-                    "language": message["language"],
-                    "code": message["code"]
+            if function_calling:
+                new_message["function_call"] = {
+                    "name": "run_code",
+                    "arguments": json.dumps({
+                        "language": message["language"],
+                        "code": message["code"]
+                    }),
+                    # parsed_arguments isn't actually an OpenAI thing, it's an OI thing.
+                    # but it's soo useful! we use it to render messages to text_llms
+                    "parsed_arguments": {
+                        "language": message["language"],
+                        "code": message["code"]
+                    }
                 }
-            }
+            else:
+                new_message["content"] += f"""\n\n```{message["language"]}\n{message["code"]}\n```"""
+                new_message["content"] = new_message["content"].strip()
 
         new_messages.append(new_message)
 
         if "output" in message:
-            output = message["output"]
-
-            new_messages.append({
-                "role": "function",
-                "name": "run_code",
-                "content": output
-            })
+            if function_calling:
+                new_messages.append({
+                    "role": "function",
+                    "name": "run_code",
+                    "content": message["output"]
+                })
+            else:
+                new_messages.append({
+                    "role": "user",
+                    "content": "CODE EXECUTED ON USERS MACHINE. OUTPUT (invisible to the user): " + message["output"]
+                })
 
     return new_messages
