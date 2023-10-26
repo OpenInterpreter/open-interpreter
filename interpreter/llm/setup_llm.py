a07cc7e26c3f3b0102a2b058bfd4f6c0472f158d
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2023-10-26 13:55:03 +0200

    Get more system info in debug mode
    This will help narrow down which systems and versions that don't work.

--- interpreter/llm/setup_llm.py
+++ interpreter/llm/setup_llm.py
@@ -5,12 +5,47 @@ from .convert_to_coding_llm import convert_to_coding_llm
 from .setup_openai_coding_llm import setup_openai_coding_llm
 import os
 import litellm
+import sys
+import platform
+import subprocess
+
+
 
 def setup_llm(interpreter):
     """
     Takes an Interpreter (which includes a ton of LLM settings),
     returns a Coding LLM (a generator that streams deltas with `message` and `code`).
     """
+
+    # Get some useful system information before starting the LLM
+    if interpreter.debug_mode:
+        
+        # Get Python version
+        python_version = sys.version
+        print(f"\nPython Version: {python_version}")
+
+        # Get pip version
+        try:
+            pip_version = subprocess.getoutput('pip --version')
+            print(f"Pip Version: {pip_version}")
+        except Exception as e:
+            print(f"Error retrieving pip version: {e}")
+
+        # Get platform (i.e., Windows, Linux, etc.s)
+        platform_name = sys.platform
+        print(f"Platform: {platform_name}")
+
+        # More detailed OS info using the platform module
+        os_name = platform.system()
+        os_release = platform.release()
+        os_version = platform.version()
+        print(f"OS Name: {os_name}")
+        print(f"OS Release: {os_release}")
+        print(f"OS Version: {os_version}")
+
+        # Print the architecture as well:
+        architecture = platform.architecture()
+        print(f"Architecture: {architecture[0]}")
 
     if (not interpreter.local
         and (interpreter.model in litellm.open_ai_chat_completion_models or interpreter.model.startswith("azure/"))):
