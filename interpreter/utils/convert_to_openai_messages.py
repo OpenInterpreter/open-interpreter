4a0765d7d16d48e5936e08c5359f0c072f77eb3f
Guilherme Ramos <guiramos@gmail.com>
2023-10-25 20:39:13 -0400

    Fixing the function output mapping when you have multiple external functions

--- interpreter/utils/convert_to_openai_messages.py
+++ interpreter/utils/convert_to_openai_messages.py
@@ -3,7 +3,7 @@ import json
 def convert_to_openai_messages(messages, function_calling=True):
     new_messages = []
 
-    for message in messages:  
+    for message in messages:
         new_message = {
             "role": message["role"],
             "content": ""
@@ -27,23 +27,36 @@ def convert_to_openai_messages(messages, function_calling=True):
                         "code": message["code"]
                     }
                 }
+
+                if "output" in message:
+                    new_message["content"] = message["output"]
+
             else:
                 new_message["content"] += f"""\n\n```{message["language"]}\n{message["code"]}\n```"""
                 new_message["content"] = new_message["content"].strip()
 
-        new_messages.append(new_message)
+            new_messages.append(new_message)
+            continue
 
         if "output" in message:
             if function_calling:
-                new_messages.append({
+                if "content" in new_message:
+                    new_messages.append(new_message)
+
+                function_name_key = next((key for key in message.keys() if isinstance(message[key], (dict, list))), None)
+
+                new_output_message = {
                     "role": "function",
-                    "name": "run_code",
                     "content": message["output"]
-                })
+                }
+                if function_name_key:
+                    new_output_message["name"] = function_name_key
+                    new_messages.append(new_output_message)
             else:
-                new_messages.append({
+                new_message = {
                     "role": "user",
                     "content": "CODE EXECUTED ON USERS MACHINE. OUTPUT (invisible to the user): " + message["output"]
-                })
+                }
+                new_messages.append(new_message)
 
     return new_messages
