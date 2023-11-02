28f796ebec7b0ffb23e40076b8df3f7d126fdc1d
Paul Farrington <pfarrington@gmail.com>
2023-11-02 17:54:06 +0000

    Pytest for powershell code exec

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,7 +1,7 @@
 import os
 import time
 from random import randint
-
+import re
 import pytest
 
 import interpreter
@@ -147,6 +147,45 @@ def test_nested_loops_and_multiple_newlines():
         """Can you write a nested for loop in python and shell and run them? Don't forget to properly format your shell script and use semicolons where necessary. Also put 1-3 newlines between each line in the code. Only generate and execute the code. No explanations. Thanks!"""
     )
 
+#@pytest.mark.skip(
+#    reason="Skipping until can verify it runs on the Github build server"
+#)
+# 
+EXPECTED_FIBONACCI_NUMBERS = [
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+    233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711,
+    28657, 46368, 75025, 121393, 196418, 317811, 514229
+]
+
+# Parameterize the test with the list of expected Fibonacci numbers
+@pytest.mark.parametrize("expected_integers", [EXPECTED_FIBONACCI_NUMBERS])
+def test_powershell_gen_exec(expected_integers):
+    TASK_RESULT_PREFIX = "TaskResult:"
+    num_fibs = str(len(expected_integers))  # Convert to string
+
+    result = interpreter.chat(
+        f"""Write a Powershell script to generate the first {num_fibs} Fibonacci numbers.
+        Approach this task methodically by planning and implementing it one step at a time.
+        Only generate and execute code - ie. do not explain, ask questions, seek assistance or offer unsolicited information.
+        Make sure to always execute the Powershell code and provide the output in the following format: {TASK_RESULT_PREFIX} x0, x1, x2,...x(n)"""
+    )
+
+    is_valid = lambda d: d.get('role') == 'assistant' and d.get('message', '').startswith(TASK_RESULT_PREFIX)
+    valid_result = next(filter(is_valid, result), None)
+    
+    if valid_result is not None:
+        message = valid_result.get('message', '')
+        pattern = r'{}\s*([\d\s,]+)'.format(re.escape(TASK_RESULT_PREFIX))  # Use re.escape() to handle any special characters
+        match = re.search(pattern, message)
+        if match:
+            integer_series = match.group(1)
+            extracted_integers = [int(num.strip()) for num in integer_series.split(',')]
+            
+            assert extracted_integers == expected_integers, "Extracted integers do not match expected integers."
+            
+            print(f"Extracted message for {num_fibs} Fibonacci numbers:", message)
+    else:
+        print(f"No valid message found in the list of results for {num_fibs} Fibonacci numbers.")
 
 def test_markdown():
     interpreter.chat(
