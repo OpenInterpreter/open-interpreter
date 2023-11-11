6d372f94b36059c920e9841dbabe31143e6d8fdc
killian <63927363+KillianLucas@users.noreply.github.com>
2023-11-11 00:24:55 -0800

    Fixing #748

--- interpreter/utils/find_image_path.py
+++ interpreter/utils/find_image_path.py
@@ -2,22 +2,9 @@ import os
 import re
 
 
-# Function to find all paths ending with .png, .jpg, or .jpeg
 def find_image_path(text):
-    # Regex pattern to find all the specified paths with extensions .png, .jpg, or .jpeg
     pattern = r"([A-Za-z]:\\[^:\n]*?\.(png|jpg|jpeg|PNG|JPG|JPEG))|(/[^:\n]*?\.(png|jpg|jpeg|PNG|JPG|JPEG))"
-
-    # Find all matches using the finditer method
-    matches = [match.group(1) for match in re.finditer(pattern, text)]
-
-    # Add to matches a version of matches where each has had "\" removed (present for some terminals)
-    matches += [match.replace("\\", "") for match in matches]
-
-    # Filter out the matches that actually exist on the filesystem
+    matches = [match.group() for match in re.finditer(pattern, text) if match.group()]
+    matches += [match.replace("\\", "") for match in matches if match]
     existing_paths = [match for match in matches if os.path.exists(match)]
-
-    # Return the longest one if any exist
-    if existing_paths:
-        return max(existing_paths, key=len)
-    else:
-        return None
+    return max(existing_paths, key=len) if existing_paths else None
