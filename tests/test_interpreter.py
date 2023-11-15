06ae76670755932b39defcb97ebf2ed414ab7937
killian <63927363+KillianLucas@users.noreply.github.com>
2023-11-14 16:31:43 -0800

    Fixed missing `end_of_message` flag, added test

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -3,14 +3,8 @@ import re
 import time
 from random import randint
 
-import pytest
-
 import interpreter
 from interpreter.utils.count_tokens import count_messages_tokens, count_tokens
-
-interpreter.auto_run = True
-interpreter.model = "gpt-3.5-turbo"
-interpreter.temperature = 0
 
 
 # this function will run before each test
@@ -27,6 +21,99 @@ def setup_function():
 # we're introducing some sleep to help avoid timeout issues with the OpenAI API
 def teardown_function():
     time.sleep(5)
+
+
+def test_hello_world():
+    hello_world_response = "Hello, World!"
+
+    hello_world_message = f"Please reply with just the words {hello_world_response} and nothing else. Do not run code. No confirmation just the text."
+
+    messages = interpreter.chat(hello_world_message)
+
+    assert messages == [
+        {"role": "user", "message": hello_world_message},
+        {"role": "assistant", "message": hello_world_response},
+    ]
+
+
+def test_math():
+    # we'll generate random integers between this min and max in our math tests
+    min_number = randint(1, 99)
+    max_number = randint(1001, 9999)
+
+    n1 = randint(min_number, max_number)
+    n2 = randint(min_number, max_number)
+
+    test_result = n1 + n2 * (n1 - n2) / (n2 + n1)
+
+    order_of_operations_message = f"""
+    Please perform the calculation `{n1} + {n2} * ({n1} - {n2}) / ({n2} + {n1})` then reply with just the answer, nothing else. No confirmation. No explanation. No words. Do not use commas. Do not show your work. Just return the result of the calculation. Do not introduce the results with a phrase like \"The result of the calculation is...\" or \"The answer is...\"
+    
+    Round to 2 decimal places.
+    """.strip()
+
+    messages = interpreter.chat(order_of_operations_message)
+
+    assert str(round(test_result, 2)) in messages[-1]["message"]
+
+
+def test_delayed_exec():
+    interpreter.chat(
+        """Can you write a single block of code and execute it that prints something, then delays 1 second, then prints something else? No talk just code. Thanks!"""
+    )
+
+
+def test_nested_loops_and_multiple_newlines():
+    interpreter.chat(
+        """Can you write a nested for loop in python and shell and run them? Don't forget to properly format your shell script and use semicolons where necessary. Also put 1-3 newlines between each line in the code. Only generate and execute the code. No explanations. Thanks!"""
+    )
+
+
+def test_write_to_file():
+    interpreter.chat("""Write the word 'Washington' to a .txt file called file.txt""")
+    assert os.path.exists("file.txt")
+    interpreter.messages = []  # Just reset message history, nothing else for this test
+    messages = interpreter.chat(
+        """Read file.txt in the current directory and tell me what's in it."""
+    )
+    assert "Washington" in messages[-1]["message"]
+
+
+def test_markdown():
+    interpreter.chat(
+        """Hi, can you test out a bunch of markdown features? Try writing a fenced code block, a table, headers, everything. DO NOT write the markdown inside a markdown code block, just write it raw."""
+    )
+
+
+def test_generator():
+    start_of_message_emitted = False
+    end_of_message_emitted = False
+    start_of_code_emitted = False
+    end_of_code_emitted = False
+    executing_emitted = False
+    end_of_execution_emitted = False
+
+    for chunk in interpreter.chat("What's 38023*40334?", stream=True, display=False):
+        print(chunk)
+        if "start_of_message" in chunk:
+            start_of_message_emitted = True
+        if "end_of_message" in chunk:
+            end_of_message_emitted = True
+        if "start_of_code" in chunk:
+            start_of_code_emitted = True
+        if "end_of_code" in chunk:
+            end_of_code_emitted = True
+        if "executing" in chunk:
+            executing_emitted = True
+        if "end_of_execution" in chunk:
+            end_of_execution_emitted = True
+
+    assert start_of_message_emitted
+    assert end_of_message_emitted
+    assert start_of_code_emitted
+    assert end_of_code_emitted
+    assert executing_emitted
+    assert end_of_execution_emitted
 
 
 def test_config_loading():
@@ -97,65 +184,3 @@ def test_token_counter():
     prompt_tokens_ok = system_tokens + prompt_tokens == prompt_token_test[0]
 
     assert system_tokens_ok and prompt_tokens_ok
-
-
-def test_hello_world():
-    hello_world_response = "Hello, World!"
-
-    hello_world_message = f"Please reply with just the words {hello_world_response} and nothing else. Do not run code. No confirmation just the text."
-
-    messages = interpreter.chat(hello_world_message)
-
-    assert messages == [
-        {"role": "user", "message": hello_world_message},
-        {"role": "assistant", "message": hello_world_response},
-    ]
-
-
-def test_math():
-    # we'll generate random integers between this min and max in our math tests
-    min_number = randint(1, 99)
-    max_number = randint(1001, 9999)
-
-    n1 = randint(min_number, max_number)
-    n2 = randint(min_number, max_number)
-
-    test_result = n1 + n2 * (n1 - n2) / (n2 + n1)
-
-    order_of_operations_message = f"""
-    Please perform the calculation `{n1} + {n2} * ({n1} - {n2}) / ({n2} + {n1})` then reply with just the answer, nothing else. No confirmation. No explanation. No words. Do not use commas. Do not show your work. Just return the result of the calculation. Do not introduce the results with a phrase like \"The result of the calculation is...\" or \"The answer is...\"
-    
-    Round to 2 decimal places.
-    """.strip()
-
-    messages = interpreter.chat(order_of_operations_message)
-
-    assert str(round(test_result, 2)) in messages[-1]["message"]
-
-
-def test_delayed_exec():
-    interpreter.chat(
-        """Can you write a single block of code and execute it that prints something, then delays 1 second, then prints something else? No talk just code. Thanks!"""
-    )
-
-
-def test_nested_loops_and_multiple_newlines():
-    interpreter.chat(
-        """Can you write a nested for loop in python and shell and run them? Don't forget to properly format your shell script and use semicolons where necessary. Also put 1-3 newlines between each line in the code. Only generate and execute the code. No explanations. Thanks!"""
-    )
-
-
-def test_write_to_file():
-    interpreter.chat("""Write the word 'Washington' to a .txt file called file.txt""")
-    assert os.path.exists("file.txt")
-    interpreter.messages = []  # Just reset message history, nothing else for this test
-    messages = interpreter.chat(
-        """Read file.txt in the current directory and tell me what's in it."""
-    )
-    assert "Washington" in messages[-1]["message"]
-
-
-def test_markdown():
-    interpreter.chat(
-        """Hi, can you test out a bunch of markdown features? Try writing a fenced code block, a table, headers, everything. DO NOT write the markdown inside a markdown code block, just write it raw."""
-    )
