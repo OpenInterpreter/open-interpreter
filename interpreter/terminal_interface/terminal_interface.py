8711fbe0a06fa4efa37d62b0f5b0330b9323e29f
CyanideByte <cyanidebyte@hotmail.com>
2023-12-05 06:26:25 -0800

    Added the fix to the CTRL+C case

--- interpreter/terminal_interface/terminal_interface.py
+++ interpreter/terminal_interface/terminal_interface.py
@@ -283,9 +283,10 @@ def terminal_interface(interpreter, message):
 
         except KeyboardInterrupt:
             # Exit gracefully
-            if active_block:
-                active_block.end()
-                active_block = None
+            if "active_block" in locals():
+                if active_block:
+                    active_block.end()
+                    active_block = None
 
             if interactive:
                 # (this cancels LLM, returns to the interactive "> " input)
