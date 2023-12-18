dffccefb5aba36628ec51bf73d64ef4f97097d32
killian <63927363+KillianLucas@users.noreply.github.com>
2023-12-18 13:44:24 -0800

    New Computer Update (improved `ast` reliability)

--- interpreter/core/computer/terminal/languages/jupyter_language.py
+++ interpreter/core/computer/terminal/languages/jupyter_language.py
@@ -251,10 +251,14 @@ def add_active_line_prints(code):
         if '"""' in line or "'''" in line:
             in_multiline_string = not in_multiline_string
         if not in_multiline_string and (line.strip().startswith("#") or line == ""):
-            whitespace = len(line) - len(line.lstrip())
+            whitespace = len(line) - len(line.lstrip(" "))
             code_lines[i] = " " * whitespace + "pass"
-    code = "\n".join(code_lines)
-    tree = ast.parse(code)
+    processed_code = "\n".join(code_lines)
+    try:
+        tree = ast.parse(processed_code)
+    except:
+        # If you can't parse the processed version, try the unprocessed version before giving up
+        tree = ast.parse(code)
     transformer = AddLinePrints()
     new_tree = transformer.visit(tree)
     return ast.unparse(new_tree)
