cabd8f9db3378bf173eb91fb3e9e7f890e799599
killian <63927363+KillianLucas@users.noreply.github.com>
2023-12-18 22:21:30 -0800

    The New Computer Update (it loves Javascript now)

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -18,8 +18,89 @@ def setup_function():
     interpreter.reset()
     interpreter.llm.temperature = 0
     interpreter.auto_run = True
-    interpreter.llm.model = "gpt-4"
+    interpreter.llm.model = "gpt-3.5-turbo"
     interpreter.debug_mode = False
+
+
+def test_generator():
+    """
+    Sends two messages, makes sure everything is correct with display both on and off.
+    """
+
+    for tests in [
+        {"query": "What's 38023*40334? Use Python", "display": True},
+        {"query": "What's 2334*34335555? Use Python", "display": True},
+        {"query": "What's 3545*22? Use Python", "display": False},
+        {"query": "What's 0.0021*3433335555? Use Python", "display": False},
+    ]:
+        assistant_message_found = False
+        console_output_found = False
+        active_line_found = False
+        flag_checker = []
+        for chunk in interpreter.chat(
+            tests["query"]
+            + "\nNo talk or plan, just immediatly code, then tell me the answer.",
+            stream=True,
+            display=tests["display"],
+        ):
+            print(chunk)
+            # Check if chunk has the right schema
+            assert "role" in chunk, "Chunk missing 'role'"
+            assert "type" in chunk, "Chunk missing 'type'"
+            if "start" not in chunk and "end" not in chunk:
+                assert "content" in chunk, "Chunk missing 'content'"
+            if "format" in chunk:
+                assert isinstance(chunk["format"], str), "'format' should be a string"
+
+            flag_checker.append(chunk)
+
+            # Check if assistant message, console output, and active line are found
+            if chunk["role"] == "assistant" and chunk["type"] == "message":
+                assistant_message_found = True
+            if chunk["role"] == "computer" and chunk["type"] == "console":
+                console_output_found = True
+            if "format" in chunk:
+                if (
+                    chunk["role"] == "computer"
+                    and chunk["type"] == "console"
+                    and chunk["format"] == "active_line"
+                ):
+                    active_line_found = True
+
+        # Ensure all flags are proper
+        assert (
+            flag_checker.count(
+                {"role": "assistant", "type": "code", "format": "python", "start": True}
+            )
+            == 1
+        ), "Incorrect number of 'assistant code start' flags"
+        assert (
+            flag_checker.count(
+                {"role": "assistant", "type": "code", "format": "python", "end": True}
+            )
+            == 1
+        ), "Incorrect number of 'assistant code end' flags"
+        assert (
+            flag_checker.count({"role": "assistant", "type": "message", "start": True})
+            == 1
+        ), "Incorrect number of 'assistant message start' flags"
+        assert (
+            flag_checker.count({"role": "assistant", "type": "message", "end": True})
+            == 1
+        ), "Incorrect number of 'assistant message end' flags"
+        assert (
+            flag_checker.count({"role": "computer", "type": "console", "start": True})
+            == 1
+        ), "Incorrect number of 'computer console output start' flags"
+        assert (
+            flag_checker.count({"role": "computer", "type": "console", "end": True})
+            == 1
+        ), "Incorrect number of 'computer console output end' flags"
+
+        # Assert that assistant message, console output, and active line were found
+        assert assistant_message_found, "No assistant message was found"
+        assert console_output_found, "No console output was found"
+        assert active_line_found, "No active line was found"
 
 
 def test_long_message():
@@ -106,86 +187,7 @@ def test_vision():
 
     interpreter.chat(messages)
 
-
-def test_generator():
-    """
-    Sends two messages, makes sure everything is correct with display both on and off.
-    """
-
-    for tests in [
-        {"query": "What's 38023*40334?", "display": True},
-        {"query": "What's 2334*34335555?", "display": True},
-        {"query": "What's 3545*22?", "display": False},
-        {"query": "What's 0.0021*3433335555?", "display": False},
-    ]:
-        assistant_message_found = False
-        console_output_found = False
-        active_line_found = False
-        flag_checker = []
-        for chunk in interpreter.chat(
-            tests["query"]
-            + "\nNo talk or plan, just immediatly code, then tell me the answer.",
-            stream=True,
-            display=tests["display"],
-        ):
-            print(chunk)
-            # Check if chunk has the right schema
-            assert "role" in chunk, "Chunk missing 'role'"
-            assert "type" in chunk, "Chunk missing 'type'"
-            if "start" not in chunk and "end" not in chunk:
-                assert "content" in chunk, "Chunk missing 'content'"
-            if "format" in chunk:
-                assert isinstance(chunk["format"], str), "'format' should be a string"
-
-            flag_checker.append(chunk)
-
-            # Check if assistant message, console output, and active line are found
-            if chunk["role"] == "assistant" and chunk["type"] == "message":
-                assistant_message_found = True
-            if chunk["role"] == "computer" and chunk["type"] == "console":
-                console_output_found = True
-            if "format" in chunk:
-                if (
-                    chunk["role"] == "computer"
-                    and chunk["type"] == "console"
-                    and chunk["format"] == "active_line"
-                ):
-                    active_line_found = True
-
-        # Ensure all flags are proper
-        assert (
-            flag_checker.count(
-                {"role": "assistant", "type": "code", "format": "python", "start": True}
-            )
-            == 1
-        ), "Incorrect number of 'assistant code start' flags"
-        assert (
-            flag_checker.count(
-                {"role": "assistant", "type": "code", "format": "python", "end": True}
-            )
-            == 1
-        ), "Incorrect number of 'assistant code end' flags"
-        assert (
-            flag_checker.count({"role": "assistant", "type": "message", "start": True})
-            == 1
-        ), "Incorrect number of 'assistant message start' flags"
-        assert (
-            flag_checker.count({"role": "assistant", "type": "message", "end": True})
-            == 1
-        ), "Incorrect number of 'assistant message end' flags"
-        assert (
-            flag_checker.count({"role": "computer", "type": "console", "start": True})
-            == 1
-        ), "Incorrect number of 'computer console output start' flags"
-        assert (
-            flag_checker.count({"role": "computer", "type": "console", "end": True})
-            == 1
-        ), "Incorrect number of 'computer console output end' flags"
-
-        # Assert that assistant message, console output, and active line were found
-        assert assistant_message_found, "No assistant message was found"
-        assert console_output_found, "No console output was found"
-        assert active_line_found, "No active line was found"
+    interpreter.force_task_completion = False
 
 
 def test_multiple_instances():
