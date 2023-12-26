9b721402085cfc66e3d2ec0f8c6a3a2d5fdf3a03
killian <63927363+KillianLucas@users.noreply.github.com>
2023-12-25 18:24:23 -0800

    New Computer Update (fixed `get_selected_text()`)

--- interpreter/core/computer/os/os.py
+++ interpreter/core/computer/os/os.py
@@ -7,11 +7,11 @@ class Os:
 
     def get_selected_text(self):
         # Store the current clipboard content
-        current_clipboard = self.computer.clipboard.paste()
+        current_clipboard = self.computer.clipboard.view()
         # Copy the selected text to clipboard
         self.computer.clipboard.copy()
         # Get the selected text from clipboard
-        selected_text = self.computer.clipboard.paste()
+        selected_text = self.computer.clipboard.view()
         # Reset the clipboard to its original content
         self.computer.clipboard.copy(current_clipboard)
         return selected_text
@@ -24,12 +24,8 @@ class Os:
             script = f'display notification "{text}" with title "{title}"'
             self.computer.terminal.run("applescript", script)
         else:  # For other OS, use a general notification API
-            try:
-                import plyer
+            import plyer
 
-                plyer.notification.notify(title=title, message=text)
-            except:
-                # This doesn't work for me, but maybe it works for other people?
-                pass
+            plyer.notification.notify(title=title, message=text)
 
     # Maybe run code should be here...?
