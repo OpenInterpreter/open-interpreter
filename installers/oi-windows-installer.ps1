5297a8299c476c3d43e3097f0f39de4af523c946
killian <63927363+KillianLucas@users.noreply.github.com>
2024-01-14 16:22:14 -0800

    Fixed Windows installer

--- installers/oi-windows-installer.ps1
+++ installers/oi-windows-installer.ps1
@@ -1,28 +1,3 @@
-# Check if Python is installed and its version is not 3.11
-$pythonVersion = & python --version 2>&1
-if ($pythonVersion -notmatch "Python 3.11") {
-    # Define the URL for the Python 3.11.7 installer
-    $url = "https://www.python.org/ftp/python/3.11.7/python-3.11.7-amd64.exe"
-    $output = "C:\Temp\python-3.11.7-amd64.exe"
-
-    # Create the Temp directory if it doesn't exist
-    if (-not (Test-Path -Path C:\Temp)) {
-        New-Item -Path C:\Temp -ItemType Directory
-    }
-
-    # Download the installer
-    Invoke-WebRequest -Uri $url -OutFile $output
-
-    # Install Python silently
-    Start-Process -FilePath $output -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait -PassThru
-
-    # Cleanup: Remove the installer file after installation
-    Remove-Item -Path $output -Force
-}
-
-# Install pyenv-win
-pip install pyenv-win
-
 # Set Python version to 3.11.7
 pyenv install 3.11.7
 pyenv global 3.11.7
