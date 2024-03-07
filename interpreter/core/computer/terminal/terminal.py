c216c2e176a9095c0866454907964592f17abf6e
Ian Jennings <ian@meetjennings.com>
2024-03-07 14:31:53 -0600

    Stop losing context when commands fail

--- interpreter/core/computer/terminal/terminal.py
+++ interpreter/core/computer/terminal/terminal.py
@@ -88,14 +88,6 @@ class Terminal:
                         chunk["recipient"] = recipient
                         chunk["content"] = content
 
-                    # Sometimes, we want to hide the traceback to preserve tokens.
-                    # (is this a good idea?)
-                    if "@@@HIDE_TRACEBACK@@@" in content:
-                        chunk["content"] = (
-                            "Stopping execution.\n\n"
-                            + content.split("@@@HIDE_TRACEBACK@@@")[-1].strip()
-                        )
-
                 yield chunk
 
                 # Print it also if display = True
