a8ee495a25a8ce348f75c18f067aa33e8b0d6bc9
killian <63927363+KillianLucas@users.noreply.github.com>
2024-03-07 03:14:26 -0800

    Teach mode

--- interpreter/core/computer/skills/skills.py
+++ interpreter/core/computer/skills/skills.py
@@ -1,6 +1,7 @@
 import glob
 import inspect
 import os
+import re
 from pathlib import Path
 
 from ....terminal_interface.utils.oi_dir import oi_dir
@@ -14,6 +15,8 @@ class Skills:
     def __init__(self, computer):
         self.computer = computer
         self.path = str(Path(oi_dir) / "skills")
+        self.new_skill = NewSkill()
+        self.new_skill.path = self.path
 
     def search(self, query):
         return aifs.search(query, self.path, python_docstrings_only=True)
@@ -26,18 +29,82 @@ class Skills:
         self.computer.save_skills = True
 
 
-class Teach:
+class NewSkill:
     def __init__(self):
+        self.path = ""
+
+    def create(self):
         self.steps = []
+        self._name = "Untitled"
         print(
-            "Welcome to the teaching session. Please add steps using .add_step('step text')"
+            """
+        
+You are creating a new skill.
+To begin, ask the user what the name of this skill is. Then, run `computer.skills.new_skill.name = "Name of the skill"`.
+        
+        """.strip()
         )
 
-    def add_step(self, step_text):
-        self.steps.append(step_text)
-        print("Step added. Run .finish() when you are done.")
+    @property
+    def name(self):
+        return self._name
 
-    def finish(self):
-        print("Teaching session finished. Here are the steps you added:")
-        for i, step in enumerate(self.steps, start=1):
-            print(f"Step {i}: {step}")
+    @name.setter
+    def name(self, value):
+        self._name = value
+        print(
+            """
+        
+You just named this skill. Great!
+Now, ask the user what the first step is, then try to execute code to accomplish that step.
+Then, ask the user if you completed the step correctly.
+Repeat this until the user said you completed the step correctly.
+Then, run `computer.skills.new_skill.add_step(step, code)` where step is a natural language description of the step, and code is the code you ran to complete it.
+If the user says the skill is complete, or that that was the last step, run `computer.skills.new_skill.save()`.
+
+              """.strip()
+        )
+
+    def add_step(self, step, code):
+        self.steps.append(step + "\n\n```python\n" + code + "\n```")
+        print(
+            """
+        
+Step added.
+Now, ask the user what the next step is, then try to execute code to accomplish that step.
+Then, ask the user if you completed the step correctly.
+Repeat this until the user said you completed the step correctly.
+Then, run `computer.skills.new_skill.add_step(step, code)` where step is a natural language description of the step, and code is the code you ran to complete it.
+If the user says the skill is complete, or that that was the last step, run `computer.skills.new_skill.save()`.
+
+        """.strip()
+        )
+
+    def save(self):
+        normalized_name = re.sub("[^0-9a-zA-Z]+", "_", self.name.lower())
+        steps_string = "\n".join(
+            [f"Step {i+1}:\n{step}\n" for i, step in enumerate(self.steps)]
+        )
+        steps_string = steps_string.replace('"""', "'''")
+        skill_string = f'''
+        
+def {normalized_name}():
+    """
+    {normalized_name}
+    """
+
+    print("To complete this task / run this skill, flexibly follow the following tutorial, swapping out parts as necessary to fulfill the user's task:\n")
+
+    print("""{steps_string}""")
+        
+        '''.strip()
+
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        with open(f"{self.path}/{normalized_name}.py", "w") as file:
+            file.write(skill_string)
+
+        print("SKILL SAVED:", self.name.upper())
+        print(
+            "Teaching session finished. Tell the user that the skill above has been saved. Great work!"
+        )
