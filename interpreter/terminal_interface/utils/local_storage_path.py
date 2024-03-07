6c5566d6e5c10191ddd8f7fff9c0da6f001642b1
Ty Fiero <tyfierodev@gmail.com>
2024-03-07 15:02:39 -0800

    Migrate older versions to 0.2.1

--- interpreter/terminal_interface/utils/local_storage_path.py
+++ interpreter/terminal_interface/utils/local_storage_path.py
@@ -3,7 +3,7 @@ import os
 import platformdirs
 
 # Using platformdirs to determine user-specific config path
-config_dir = platformdirs.user_config_dir("Open Interpreter")
+config_dir = platformdirs.user_config_dir("open-interpreter")
 
 
 def get_storage_path(subdirectory=None):
