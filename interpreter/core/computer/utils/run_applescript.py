f3048191d13b56c130264cc905a8054081184e60
killian <63927363+KillianLucas@users.noreply.github.com>
2024-03-14 14:34:54 -0700

    Loud applescript, 01 system message improvements

--- interpreter/core/computer/utils/run_applescript.py
+++ interpreter/core/computer/utils/run_applescript.py
@@ -1,10 +1,15 @@
 import subprocess
 
+
 def run_applescript(script):
     """
     Runs the given AppleScript using osascript and returns the result.
     """
-    args = ['osascript', '-e', script]
+    print("Running this AppleScript:\n", script)
+    print(
+        "---\nFeel free to directly run AppleScript to accomplish the user's task. This gives you more granular control than the `computer` module, but it is slower."
+    )
+    args = ["osascript", "-e", script]
     return subprocess.check_output(args, universal_newlines=True)
 
 
@@ -12,7 +17,11 @@ def run_applescript_capture(script):
     """
     Runs the given AppleScript using osascript, captures the output and error, and returns them.
     """
-    args = ['osascript', '-e', script]
+    print("Running this AppleScript:\n", script)
+    print(
+        "---\nFeel free to directly run AppleScript to accomplish the user's task. This gives you more granular control than the `computer` module, but it is slower."
+    )
+    args = ["osascript", "-e", script]
     result = subprocess.run(args, capture_output=True, text=True, check=False)
     stdout, stderr = result.stdout, result.stderr
     return stdout, stderr
