e5fdc57726869e2149e6cef36b287a957a58d507
killian <63927363+KillianLucas@users.noreply.github.com>
2024-03-21 12:18:11 -0700

    Fixed rare skill issue

--- interpreter/core/computer/skills/skills.py
+++ interpreter/core/computer/skills/skills.py
@@ -24,7 +24,20 @@ class Skills:
 
     def import_skills(self):
         previous_save_skills_setting = self.computer.save_skills
+
         self.computer.save_skills = False
+
+        # Make sure it's not over 100mb
+        total_size = 0
+        for path, dirs, files in os.walk(self.path):
+            for f in files:
+                fp = os.path.join(path, f)
+                total_size += os.path.getsize(fp)
+        total_size = total_size / (1024 * 1024)  # convert bytes to megabytes
+        if total_size > 100:
+            raise Warning(
+                f"Skills at path {self.path} can't exceed 100mb. Try deleting some."
+            )
 
         code_to_run = ""
         for file in glob.glob(os.path.join(self.path, "*.py")):
@@ -34,7 +47,24 @@ class Skills:
         if self.computer.interpreter.debug:
             print("IMPORTING SKILLS:\n", code_to_run)
 
-        self.computer.run("python", code_to_run)
+        output = self.computer.run("python", code_to_run)
+
+        if "traceback" in output.lower():
+            # Import them individually
+            for file in glob.glob(os.path.join(self.path, "*.py")):
+                with open(file, "r") as f:
+                    code_to_run = f.read() + "\n"
+
+                if self.computer.interpreter.debug:
+                    print("IMPORTING SKILL:\n", code_to_run)
+
+                output = self.computer.run("python", code_to_run)
+
+                if "traceback" in output.lower():
+                    print(
+                        f"Skill at {file} might be broken— it produces a traceback when run."
+                    )
+
         self.computer.save_skills = previous_save_skills_setting
 
 
