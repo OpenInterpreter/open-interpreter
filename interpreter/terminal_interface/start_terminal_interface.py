4d852df0f9686c2dad3eab529e2cd62b80739f07
Steve Wang <steve235lab@hotmail.com>
2024-03-21 19:02:43 +0800

    Match model names with str.startwith instead of trying to enumerate all known models.

--- interpreter/terminal_interface/start_terminal_interface.py
+++ interpreter/terminal_interface/start_terminal_interface.py
@@ -357,30 +357,15 @@ def start_terminal_interface(interpreter):
 
     ### Set some helpful settings we know are likely to be true
 
-    if interpreter.llm.model in [
-        "gpt-4-turbo-preview",
-        "gpt-4-0125-preview",
-        "gpt-4-1106-preview",
-        "openai/gpt-4-turbo-preview",
-        "openai/gpt-4-0125-preview",
-        "openai/gpt-4-1106-preview",
-    ]:
+    if interpreter.llm.model.startswith("gpt-4") or interpreter.llm.model.startswith("openai/gpt-4"):
         if interpreter.llm.context_window is None:
             interpreter.llm.context_window = 128000
         if interpreter.llm.max_tokens is None:
             interpreter.llm.max_tokens = 4096
         if interpreter.llm.supports_functions is None:
-            interpreter.llm.supports_functions = True
+            interpreter.llm.supports_functions = False if "vision" in interpreter.llm.model else True
 
-    if interpreter.llm.model in ["gpt-4-1106-vision-preview", "openai/gpt-4-1106-vision-preview"]:
-        if interpreter.llm.context_window is None:
-            interpreter.llm.context_window = 128000
-        if interpreter.llm.max_tokens is None:
-            interpreter.llm.max_tokens = 4096
-        if interpreter.llm.supports_functions is None:
-            interpreter.llm.supports_functions = False
-
-    if interpreter.llm.model in ["gpt-3.5-turbo-1106", "openai/gpt-3.5-turbo-1106"]:
+    if interpreter.llm.model.startswith("gpt-3.5-turbo") or interpreter.llm.model.startswith("openai/gpt-3.5-turbo"):
         if interpreter.llm.context_window is None:
             interpreter.llm.context_window = 16000
         if interpreter.llm.max_tokens is None:
