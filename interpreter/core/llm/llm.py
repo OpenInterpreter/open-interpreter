69dc8e6b1176d102b2c39759ad9d3f1eb5f0fd8f
killian <63927363+KillianLucas@users.noreply.github.com>
2024-05-12 19:41:18 -0700

    Better messaging

--- interpreter/core/llm/llm.py
+++ interpreter/core/llm/llm.py
@@ -234,7 +234,7 @@ def fixed_litellm_completions(**params):
 
     if "local" in params.get("model"):
         # Kinda hacky, but this helps
-        params["stop"] = ["<|assistant|>", "<|end|>"]
+        params["stop"] = ["<|assistant|>", "<|end|>", "<|eot_id|>"]
 
     # Run completion
     first_error = None
